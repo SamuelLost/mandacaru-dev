41da061253c3ab06d71b18885bdba1e92fd928e2
Samuel Henrique <samuelhenriqec@alu.ufc.br>
2022-07-25 17:03:57 -0300

    início da aula de Thymeleaf

---
+++ MandacaruThymeleaf/src/main/java/br/ufc/mandacaru/Hello.java
@@ -0,0 +1,53 @@
+package br.ufc.mandacaru;
+
+import java.io.IOException;
+
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+import org.thymeleaf.web.IWebExchange;
+import org.thymeleaf.web.servlet.JakartaServletWebApplication;
+
+import br.ufc.mandacaru.config.ThymeleafConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Hello
+ */
+public class Hello extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Hello() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		TemplateEngine templateEngine = (TemplateEngine) getServletContext()
+				.getAttribute(ThymeleafConfig.TEMPLATE_ENGINE_ATTR);
+		IWebExchange webExchange = JakartaServletWebApplication.buildApplication(getServletContext())
+				.buildExchange(request, response);
+		WebContext context = new WebContext(webExchange);
+		context.setVariable("name", "Samuel Henrique");
+		context.setVariable("hello", "Hello World");
+		templateEngine.process("hello", context, response.getWriter());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
