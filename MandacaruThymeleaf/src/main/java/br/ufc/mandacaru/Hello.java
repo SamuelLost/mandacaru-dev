be38305bb68b2f07c72cc0cdbdad65f01bafbd43
Samuel Henrique <samuelhenriqec@alu.ufc.br>
2022-07-26 16:02:25 -0300

    fim da aula de Thymeleaf

--- MandacaruThymeleaf/src/main/java/br/ufc/mandacaru/Hello.java
+++ MandacaruThymeleaf/src/main/java/br/ufc/mandacaru/Hello.java
@@ -1,6 +1,8 @@
 package br.ufc.mandacaru;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
@@ -8,6 +10,8 @@ import org.thymeleaf.web.IWebExchange;
 import org.thymeleaf.web.servlet.JakartaServletWebApplication;
 
 import br.ufc.mandacaru.config.ThymeleafConfig;
+import br.ufc.mandacaru.model.Address;
+import br.ufc.mandacaru.model.Person;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -37,8 +41,20 @@ public class Hello extends HttpServlet {
 		IWebExchange webExchange = JakartaServletWebApplication.buildApplication(getServletContext())
 				.buildExchange(request, response);
 		WebContext context = new WebContext(webExchange);
-		context.setVariable("name", "Samuel Henrique");
-		context.setVariable("hello", "Hello World");
+		Address address1 = new Address("Rua Zacarias Carlos de Melo", "63700-000");
+		Person person1 = new Person(1, "Almada", address1);
+		Address address2 = new Address("Rua das Oiticicas", "60700-000");
+		Person person2 = new Person(2, "Anderson", address2);
+		Address address3 = new Address("Rua das Carnaubas", "60000-000");
+		Person person3 = new Person(3, "Francisco", address3);
+		List<Person> people = new ArrayList<Person>();
+		people.add(person1);
+		people.add(person2);
+		people.add(person3);
+		context.setVariable("people", people);
+		
+//		context.setVariable("name", "Samuel Henrique");
+//		context.setVariable("hello", "Hello World");
 		templateEngine.process("hello", context, response.getWriter());
 	}
 
